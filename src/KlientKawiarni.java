fc489aaea9784c8ec4f8858a506af70e72b5cd7d
Maciekj103 <maciek.jablkowski06@gmail.com>
2026-03-25 08:54:46 +0100

    Dodano pola

---
+++ src/KlientKawiarni.java
@@ -0,0 +1,8 @@
+public class KlientKawiarni {
+
+    private int idKlienta;
+    private String imie;
+    private String nazwisko;
+    private String email;
+
+}
---
+++ src/ProduktMenu.java
@@ -0,0 +1,10 @@
+public class ProduktMenu {
+
+    private String kod;
+    private String nazwa;
+    private double cena;
+    private String kategoria;
+    private static int liczbaProduktow;
+
+
+}
